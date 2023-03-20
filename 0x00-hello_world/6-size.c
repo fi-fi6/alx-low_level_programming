951e0e14618bc65421145e07b04910e775815e4e
fi-fi6 <focusalx@gmail.com>
2023-03-20 04:17:59 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,6 @@
  * main - a program that prints the size of types
  * Return:  Always 0 (Success), non zero otherwise
  */
-
 int main(void)
 {
 	printf("Size of a char: %ld byte(s) \n", sizeof(char));
@@ -12,7 +11,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
-
 	return (0);
 }
 
