8289abd1f10da2d2aa0979c9151105d433ce1544
fi-fi6 <focusalx@gmail.com>
2023-04-02 15:43:58 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute an absolute value of an integer
+ *@n: a number to compute
+ *Return: absolute number
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
